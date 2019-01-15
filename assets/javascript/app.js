2c9e9b0b9e75d8adfe4b311bf694ab80c927dc83
Olivia Fontanese <olfo4481@colorado.edu>
2019-01-14 18:33:46 -0700

    timer not stopping but other stuff seems functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,15 @@
+// import { createDecipher } from "crypto";
+// import { createReadStream } from "fs";
+
 $("#start").on("click",function(){
     game.start();
-})
+});
+
+$("#start").on("click",function(){
+    game.tally();
+});
+
+
 
 var questions = [{
     question:"first full length movie?",
@@ -22,43 +31,56 @@ var questions = [{
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 20,
+    counter:4,
     countdown: function(){
         game.counter--;
         $("#counter").html(game.counter);
         if(game.counter<=0){
             console.log("time's up!");
-            game.done();
+            game.tally();
         }
     },
     start: function(){
         timer = setInterval(game.countdown,1000);
-        $("#subwrapper").prepend('<h2>Time remaining: <span id="counter">20</span> Seconds</h2>')
+
+        $("#subwrapper").prepend(
+            '<h2>Time remaining: <span id="counter">4</span> Seconds</h2>'
+            );
+
         $("#start").remove(); // we don't need the start button once we've already started
-        for(var i=0; i<questions.length; i++){
+
+        for(var i = 0; i < questions.length; i++){
             $("#subwrapper").append('<h2>'+questions[i].question + '</h2>');
             for(var j=0; j<questions[i].answers.length; j++){
                 $("#subwrapper").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]);
             }
-        }
+        } 
+        
     },
-    done: function(){
+
+    timer: function(){
+        if (time === 0){
+            clearInterval(counter);
+            $("#counter").clear();
+        } counter--;
+    },
+    tally: function(){
         $.each($('input[name="question-0]":checked'), function(){
-            if($(this).val()==questions[0].correctAnswer){
+            if($(this).val() == questions[0].correctAnswer){
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
         $.each($('input[name="question-1]":checked'), function(){
-            if($(this).val()==questions[1].correctAnswer){
+            if($(this).val() == questions[1].correctAnswer){
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
         $.each($('input[name="question-2]":checked'), function(){
-            if($(this).val()==questions[2].correctAnswer){
+            if($(this).val() == questions[2].correctAnswer){
                 game.correct++;
             } else {
                 game.incorrect++;
@@ -69,9 +91,10 @@ var game = {
     result: function(){
         clearInterval(timer);
         $('#subwrapper h2').remove();
-        $('#subwrapper').html("<h2>All done!</h2>");
-        $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
-        $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
-        $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</3>");
+        $('#subwrapper').html("<h2>All tally!</h2>");
+        $('#subwrapper').append("<h3>Correct Answers: " + this.correct + "</h3>");
+         $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect + "</h3>");
+        $('#subwrapper').append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</3>");
     }
 }
+
